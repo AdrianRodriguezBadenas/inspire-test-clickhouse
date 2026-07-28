fc1422b3d72037109fb705b74a31a38a5ff8573c
Adrian Rodriguez <adrian.rodriguez@genomcore.com>
2026-07-28 07:56:32 +0200

    delete code

--- source/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { AnalyticsModule } from './analytics/analytics.module';
-import { buildGraphqlConfig } from './graphql.config';
-
-@Module({
-  imports: [GraphQLModule.forRoot(buildGraphqlConfig()), AnalyticsModule],
-})
-export class AppModule {}
